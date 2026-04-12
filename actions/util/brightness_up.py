015aafd40b6e3f10b42e5cf35c5283a28d33e388
GodKode69 <knownasraghav@gmail.com>
2026-04-13 03:05:12 +0530

    implemented 2 actions based on the new structure - brightness up and down

---
+++ actions/util/brightness_up.py
@@ -0,0 +1,27 @@
+import subprocess
+from actions.base import BaseAction
+from actions.system import system
+
+class BrightnessUp(BaseAction):
+    name = "Brightness Up"
+    description = "Increase system brightness"
+    id = "brightness_up"
+
+    def execute(self) -> None:
+        sys = system()
+        
+        if sys=="linux":
+                try:
+                    import subprocess
+                    subprocess.Popen(
+                        ["brightnessctl", "set", "5%-"],
+                    stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL,
+                    )
+                except Exception as exc:
+                    print(f"[{self.id}] {exc}")
+        elif sys=="win":
+            #code here
+            print("hi")
+        elif sys=="mac":
+            #code here
+            print("hie")
